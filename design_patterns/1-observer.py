8a190635df511d206424583c9f470d73aa796c35
olium <jeremie.rouxel@outlook.fr>
2026-05-21 04:36:38 +0200

    .

--- design_patterns/1-observer.py
+++ design_patterns/1-observer.py
@@ -11,9 +11,10 @@ class NewsSubject:
     def __init__(self) -> None:
         self._subs: dict[Observer, set[str] | None] = {}
 
-    def subscribe(self, observer: Observer, topics: set[str] | None = None) -> None:
+    def subscribe(self, observer: Observer,
+                  topics: set[str] | None = None) -> None:
         if observer in self._subs:
-            return  # ignore duplicate subscribe for same instance
+            return
         self._subs[observer] = topics
 
     def unsubscribe(self, observer: Observer) -> None:
@@ -60,4 +61,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-    
